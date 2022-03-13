f31e642361736fca644ba32fd44474a7fd1527b1
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-03-12 19:31:43 -0800

    Created user db

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'JSM'
+
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/user'
 
     from .views import views
     from .auth import auth
